6cec8f72e309bda2fe6e34caa4e0f41f52803256
essam <esamfarg91@gmail.com>
2020-05-18 23:51:24 +0200

    remove the baseUrl in editor

--- src/components/MainViews/Editor.js
+++ src/components/MainViews/Editor.js
@@ -26,7 +26,7 @@ const Editor = (props) => {
       var pixie = new window.Pixie({
         crossOrigin: true,
         watermarkText: 'Memer.ly',
-        baseUrl: '/',
+        // baseUrl: '/',
         onLoad: function () {
           window.postMessage('pixieLoaded', '*');
         },
